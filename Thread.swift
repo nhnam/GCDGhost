9f4530a9470aac9416b5fc12423ac19f9afa4ff3
Alvin Varghese <alvins@Alvins-MacBook-Pro.local>
2016-08-01 00:56:12 +0530

    Converted to Swift3

--- Thread.swift
+++ Thread.swift
@@ -2,51 +2,35 @@
 import Foundation
 import UIKit
 
-struct QualityOfServiceClasses
-{
-    static let Interactive = QOS_CLASS_USER_INTERACTIVE
-    static let Initiated = QOS_CLASS_USER_INITIATED
-    static let Utility = QOS_CLASS_UTILITY
-    static let Background = QOS_CLASS_BACKGROUND
-    static let Default = QOS_CLASS_DEFAULT
-    static let Unspecified = QOS_CLASS_UNSPECIFIED
-}
-
-
 public class DSL  {
     
-    class func mainQueue() -> dispatch_queue_t {
-        return dispatch_get_main_queue()
+    class func mainQueue() -> DispatchQueue {
+        return DispatchQueue.main
     }
     
-    class func userInteractiveQueue() -> dispatch_queue_t {
+    class func userInteractiveQueue() -> DispatchQueue {
         
-        return dispatch_get_global_queue(QualityOfServiceClasses.Interactive, 0)
+        return DispatchQueue.global(attributes: DispatchQueue.GlobalAttributes.qosUserInteractive)
     }
     
-    class func userInitiatedQueue() -> dispatch_queue_t {
+    class func userInitiatedQueue() -> DispatchQueue {
         
-        return dispatch_get_global_queue(QualityOfServiceClasses.Initiated, 0)
+        return DispatchQueue.global(attributes: DispatchQueue.GlobalAttributes.qosUserInitiated)
     }
     
-    class func utilityQueue() -> dispatch_queue_t {
+    class func utilityQueue() -> DispatchQueue {
         
-        return dispatch_get_global_queue(QualityOfServiceClasses.Utility, 0)
+        return DispatchQueue.global(attributes: DispatchQueue.GlobalAttributes.qosUtility)
     }
     
-    class func backgroundQueue() -> dispatch_queue_t {
+    class func backgroundQueue() -> DispatchQueue {
         
-        return dispatch_get_global_queue(QualityOfServiceClasses.Background, 0)
+        return DispatchQueue.global(attributes: DispatchQueue.GlobalAttributes.qosBackground)
     }
     
-    class func defaultQueue() -> dispatch_queue_t {
+    class func defaultQueue() -> DispatchQueue {
         
-        return dispatch_get_global_queue(QualityOfServiceClasses.Default, 0)
-    }
-    
-    class func unSpecifiedQueue() -> dispatch_queue_t {
-        
-        return dispatch_get_global_queue(QualityOfServiceClasses.Unspecified, 0)
+        return DispatchQueue.global(attributes: DispatchQueue.GlobalAttributes.qosDefault)
     }
 }
 
@@ -54,65 +38,58 @@ class Thread {
     
     //MARK: Asynchronous Queue
     
-    class func async(name : String,block : dispatch_block_t) -> () {
+    class func async(_ name : String,block : ()->()) -> () {
         
-        return  dispatch_async(dispatch_queue_create(name, nil), block)
+        return  DispatchQueue(label: name, attributes: []).async(execute: block)
     }
     
     //MARK: Asynchronous Queue With Attributes
     
-    class func async(name : String, attribute : dispatch_queue_attr_t, block : dispatch_block_t) -> () {
+    class func async(_ name : String, attribute : DispatchQueueAttributes, block : ()->()) -> () {
         
-        return dispatch_async(dispatch_queue_create(name, nil), block)
+        return DispatchQueue(label: name, attributes: []).async(execute: block)
     }
     
     //MARK: Main Queue
     
-    class func main(block : dispatch_block_t) -> () {
+    class func main(_ block : ()->()) -> () {
         
-        return dispatch_async(DSL.mainQueue(), block)
+        return DSL.mainQueue().async(execute: block)
     }
     
     //MARK: Background Queue
     
-    class func background(block : dispatch_block_t) -> () {
+    class func background(_ block : ()->()) -> () {
         
-        return dispatch_async(DSL.backgroundQueue(), block)
+        return DSL.backgroundQueue().async(execute: block)
     }
     
     //MARK: Utility Queue
     
-    class func utility(block : dispatch_block_t) -> () {
+    class func utility(_ block : ()->()) -> () {
         
-        return dispatch_async(DSL.utilityQueue(), block)
+        return DSL.utilityQueue().async(execute: block)
     }
     
     //MARK: User Interactive Queue
     
-    class func userInteractive(block : dispatch_block_t) -> () {
+    class func userInteractive(_ block : ()->()) -> () {
         
-        return dispatch_async(DSL.userInitiatedQueue(), block)
+        return DSL.userInitiatedQueue().async(execute: block)
     }
     
     //MARK: User Initiated Queue
     
-    class func userInitiated(block : dispatch_block_t) -> () {
+    class func userInitiated(_ block : ()->()) -> () {
         
-        return dispatch_async(DSL.userInitiatedQueue(), block)
-    }
-    
-    //MARK: Unspecified Queue
-    
-    class func unspecified(block : dispatch_block_t) -> () {
-        
-        return dispatch_async(DSL.unSpecifiedQueue(), block)
+        return DSL.userInitiatedQueue().async(execute: block)
     }
     
     //MARK: Default Queue
     
-    class func defualt(block : dispatch_block_t) -> () {
+    class func defualt(_ block : ()->()) -> () {
         
-        return dispatch_async(DSL.defaultQueue(), block)
+        return DSL.defaultQueue().async(execute: block)
     }
 }
 
